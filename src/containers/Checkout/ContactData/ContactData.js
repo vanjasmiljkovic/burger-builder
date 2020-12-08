c7bfe981c783ef60cb330e659d1cbcef8a450707
Vanja Smiljkovic <vanjasmiljkovic22@gmail.com>
2020-12-08 21:06:26 +0100

    Added exclamation mark

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -185,7 +185,7 @@ class ContactData extends Component {
         }
         return(
             <div className={classes.ContactData}>
-                <h4>Enter your Contact Data</h4>
+                <h4>Enter your Contact Data!</h4>
                 {form}
             </div>
         );
